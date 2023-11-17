71863f8a4e19f07ca326259eb1018ef4b08a5660
Yossakorn Napeang <ynt29tie@gmail.com>
2023-11-17 16:34:35 +0700

    add google fonts add vuetify add color variables

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-btn>
+      Overpass
+    </v-btn>
+    <p class="text-test">ทดสอบ</p>
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/_variables.scss";
+
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  app: {
+    head: {
+      charset: "UTF-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Yossakorn Napeang Web Portfolio"
+    }
+  },
+  css: [
+      'vuetify/lib/styles/main.sass',
+      '@/assets/css/global.css',
+      '@/assets/scss/_variables.scss'
+    ],
+    
+  build: {
+    transpile: ['vuetify']
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Overpass: [300, 500, 700]
+    },
+    useStylesheet: true
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    },
+  }
 })
