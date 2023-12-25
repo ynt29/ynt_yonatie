3923cd0fbec492df0f62183a81befbf0681365d4
Yossakorn Napeang <ynt29tie@gmail.com>
2023-12-25 14:58:59 +0700

    add github pages config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/ynt_yonatie',
     head: {
       charset: "UTF-8",
       viewport: "width=device-width, initial-scale=1",
